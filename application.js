d9637f45cef0bbb2a21d4b0d060e4a1af03701ae
AJ NOURI <ajn@localhost.localdomain>
2017-03-09 10:46:02 +0100

    last updates

---
+++ application.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	var sources = new Array()
+
+	sources[0] = 'https://h5p.org/h5p/embed/19684'
+	sources[1] = 'https://h5p.org/h5p/embed/21468'
+	sources[2] = 'https://h5p.org/h5p/embed/21469'
+	sources[3] = 'https://h5p.org/h5p/embed/21470'
+
+	var p = sources.length;
+
+	$('#randomize').click(function() {
+	    var randomSource = Math.round(Math.random()*(p-1));
+	    //console.log(sources[randomSource]);
+	    $('#question').attr('src', sources[randomSource]);
+	 	//$("#question").text("URL value is now: " + sources[randomSource]);    
+    });
+	$('#memor1').click(function() {
+	    $('#memorejo').attr('src', 'https://h5p.org/h5p/embed/21988');
+	});
+	$('#memor2').click(function() {
+	    $('#memorejo').attr('src', 'https://h5p.org/h5p/embed/21992');
+	});
+	$('#memor3').click(function() {
+	    $('#memorejo').attr('src', 'https://h5p.org/h5p/embed/24741');
+	});
+});
+
